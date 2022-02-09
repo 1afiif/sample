e8230def6afd1387e40517dfa67f9965858c9a57
iamafif <khayotullokh00057@gmail.com>
2022-02-09 21:32:22 +0900

    deleted all code

--- index.html/index.js
+++ index.html/index.js
@@ -1,14 +1,1 @@
-// Date of solution 2022.02.09
-
-function alphabetPosition(text) {
-    return text
-        .toUpperCase()
-        .match(/[a-z]/gi)
-        .map((c) => c.charCodeAt() - 64)
-        .join(' ');
-}
-
-console.log(alphabetPosition('Hello World is not enough for us '));
-
-// Date of solution 2022.02.01
 
