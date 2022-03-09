455b0266738d0ebcab615b6494f3cbae974243a7
iamafif <khayotullokh00057@gmail.com>
2022-03-10 03:17:31 +0900

    solution ready check this out

--- index.html/index.js
+++ index.html/index.js
@@ -110,6 +110,30 @@
 
 // Date of Solution 2022.03.10 Return Largest Numbers in Array 
 
+function largestOfFour(arr) {
+  let results = [];
+  for (let i = 0; i < arr.length; i++) {
+    let largestNumber = arr[i][0];
+    // console.log(largestNumber);
+    for (let j = 1; j < arr[i].length; j++) {
+      if (arr[i][j] > largestNumber) {
+        largestNumber = arr[i][j];
+      }
+    }
+    results[i] = largestNumber;
+  }
+  return results;
+}
+
+
+console.log(
+  largestOfFour([
+    [4, 5, 1, 3],
+    [13, 27, 18, 26],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1],
+  ])
+);
 
 
 
